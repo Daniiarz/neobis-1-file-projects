faaa2358bddfc46e1ee4e88109d9ff7a5037356b
Daniiarz <daniyarflash.m01@gmail.com>
2019-09-16 19:33:47 +0600

    Initial Commit

---
+++ js/sample_json.js
@@ -0,0 +1,12 @@
+let sample_python_backend_student_pro_max_plus_extra_cool = {
+    "student_name": "Daniiar",
+    "mentor": {
+            "mentor": "Аббас",
+            "tg_mentor": "@Abbas10r"
+    },
+    "tg_student": "@Daniiarz",
+    "university": "Ala-Too",
+    "course": 2,
+    "missed_report": 0,
+    "missed_projects": 0
+}
