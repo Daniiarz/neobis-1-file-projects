9a5e01156ae5c252783dc4ff7ea91fefb5b791dd
Daniiarz <daniyarflash.m01@gmail.com>
2019-09-18 15:06:29 +0600

    Solved some python problems

---
+++ python/#2/calculations_a.py
@@ -0,0 +1,8 @@
+import re
+
+a = input()
+n_l = list(map(int, re.findall("\w", a)))
+
+print("{}+{}+{}={}".format(n_l[0], n_l[1], n_l[2], sum(n_l)))
+print("{}*{}*{}={}".format(n_l[0], n_l[1], n_l[2], n_l[0]*n_l[1]*n_l[2]))
+print("({}+{}+{})/{}={}".format(n_l[0], n_l[1], n_l[2], len(n_l), (sum(n_l) / len(n_l))))
